6dda14fe891dad055d77e7b2b705b9d58248e17a
savasgormus <savasgormus1@gmail.com>
2022-04-07 22:41:17 +0300

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import { Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles'
+// import { GiHamburgerMenu } from "react-icons/gi"
 
 const Navbar = () => {
+  const [isOpen,setIsOpen] = useState(false);
   return (
-    <div>READY PROJECT</div>
+    <Nav>
+
+      <Logo to="/home">
+        <i>{"<Clarusway>"}</i>
+        <span> Recipe</span>
+      </Logo>
+
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <span/>
+        <span/>
+        <span/>
+        {/* <GiHamburgerMenu/> */}
+      </Hamburger>
+
+      <Menu menubar={isOpen}>
+        <MenuLink to="/about" onClick={()=>setIsOpen(!isOpen)}>About</MenuLink>
+        <MenuLink to="/github">Github</MenuLink>
+        <MenuLink to="/" onClick={()=>setIsOpen(!isOpen)}>Logout</MenuLink>
+      </Menu>
+
+
+    </Nav>
   )
 }
 
